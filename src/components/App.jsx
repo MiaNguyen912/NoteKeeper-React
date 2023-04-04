7e9f03eb68cb0ac6f4b29d0ec613a6deeeffdfd6
Mia Nguyen <nguyendanlinh9122000@gmail.com>
2023-04-04 04:39:41 -0700

    use hook and event handlers to deal with button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
-
+import CreateArea from "./CreateArea";
 
 
 function App(){
+    const [list, setList] = useState([])
+
+    function addNote(newNote){
+        setList(prevList => {
+            return [...prevList, newNote]
+        })
+    }
+    function deleteNote(id){
+        setList((prevList) => {
+            return prevList.filter((note, index) => {
+              return index !== id;
+            }); //return a new array without the note with the specified id
+        });
+    }
+
     return (
         <>
             <Header/>
-            {notes.map((item) => {
+            <CreateArea
+                onAdd={addNote}
+            />
+            {list.map((item, index)=> {
                 return(
-                    <Note
-                        key={item.key}
-                        title= {item.title}  
-                        content={item.content}
-                    />
+                  <Note 
+                    key={index} 
+                    id={index}
+                    title= {item.title} 
+                    content={item.content}
+                    onDelete={deleteNote}
+                  />
                 )
             })}
-            
             <Footer/>
         </>
     );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+
+function CreateArea(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setNote(prevNote => {
+            return {...prevNote, [name]: value};
+        })
+    }
+    function handleSubmit(event){
+        event.preventDefault();
+        props.onAdd(note)
+        setNote({
+            title: "",
+            content: ""
+        })
+    }
+   
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+            onChange={handleChange} 
+            name="title" 
+            value={note.title}
+            placeholder="Title" 
+        />
+        <textarea 
+            onChange={handleChange} 
+            name="content" 
+            value={note.content}
+            placeholder="Take a note..." rows="3" 
+        />
+        <button 
+            type="submit"
+        >Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
